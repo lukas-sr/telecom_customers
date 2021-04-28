df7a96f98c94887c36de61a678cbd4f1bb136fc3
lukas-sr <lukas-sr@hotmail.com>
2021-04-28 12:45:47 -0300

    added graphics with plotly lib

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Data Analysis of Churns on the telecomunications company
 import pandas as pd
 import plotly.express as px
+from IPython.display import display
 
 df = pd.read_csv("telecom_users.csv")
 
@@ -9,10 +10,19 @@ print("After Data Processing\n")
 print(df.info())
 
 # Data processing
-df = df.drop("Unnamed: 0", axis="columns")
+df = df.drop("Unnamed: 0", axis=1)
 df["TotalGasto"] = pd.to_numeric(df["TotalGasto"], errors="coerce")
-df = df.dropna(how="all", axis="columns")
+df = df.dropna(how="all", axis=1)
 df = df.dropna(how="any", axis=0)
 
 print("\nBefore Data Processing\n")
 print(df.info())
+
+# Checking the percentage of Churns in the Dataframe -> Around 26%
+display(df["Churn"].value_counts(normalize=True).map("{:.1%}".format))
+
+# Visualizing the data
+for column in df:
+    grap = px.histogram(df, x=column, color="Churn")
+    grap.show()
+
